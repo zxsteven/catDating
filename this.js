58daee5a28e18ef9779c1eb012e0e6ea33887e0b
Steven Buxton <breathesteve@gmail.com>
2017-02-01 07:54:01 -0600

    Finished
    Cat Dating app layout

---
+++ this.js
@@ -0,0 +1,5 @@
+var name1 = prompt("What's your first name?");
+var name2 = prompt("What's your last name?");
+var age = prompt('How old are you?');
+console.log("Your name is " + name1 + " " + name2 + ".");
+console.log("Your age is " + age + " years old.");
